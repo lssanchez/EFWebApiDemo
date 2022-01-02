897dcf285962dce6c27cae986c4759b072faa885
raven <raven@mail.com>
2022-01-02 02:08:00 -0600

    feature-AddEntityPelicula

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -15,6 +15,7 @@ namespace EFWebApiDemo
         public DbSet<Genero> Generos { get; set; }
         public DbSet<Actor> Actores { get; set; }
         public DbSet<Cine> Cines { get; set; }
+        public DbSet<Pelicula> Peliculas { get; set; }
         #endregion
 
     }
---
+++ Entidades/Pelicula.cs
@@ -0,0 +1,8 @@
+namespace EFWebApiDemo.Entidades
+{
+    public class Pelicula
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
